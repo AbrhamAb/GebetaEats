9631f9e89dbc587c0cf9e41f724eb819597148c3
Zelalem Getahun <zelalemg935@gmail.com>
2025-12-08 02:29:44 -0800

    file structure and running first demo screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/screens/home/home_screen.dart';
 
 void main() {
   runApp(const GebetaEatsApp());
@@ -13,11 +14,7 @@ class GebetaEatsApp extends StatelessWidget {
       title: 'GebetaEats',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
-      // home: const HomeScreen(),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('GebetaEats')),
-        body: const Center(child: Text('Hello, GebetaEats!')),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
